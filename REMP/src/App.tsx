365c876c531ff31d913855ecb4e042d689480212
Elijah925 <79500501+Elijah925@users.noreply.github.com>
2025-05-03 20:08:46 +1000

    Update App.tsx

--- REMP/src/App.tsx
+++ REMP/src/App.tsx
@@ -2,9 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +9,14 @@ function App() {
   return (
     <>
     <h1 className="text-3xl font-bold text-blue-600">Hello Tailwind</h1>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -21,23 +26,9 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={
-            <div>
-              <Link to="/About" className="text-blue-500 hover:underline">Navigate to the about Page</Link>
-              <Home/>
-            </div>
-          } />
-
-          <Route path="/about" element={
-            <div>
-              <Link to="/" className="text-blue-500 hover:underline">Navigate to the Home Page</Link>
-              <About/>
-            </div>}/>
-        </Routes>
-    </BrowserRouter>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
