ff3de78a35fc7905d53bba191cc1e9921d06d97a
Elijah925 <79500501+Elijah925@users.noreply.github.com>
2025-05-03 16:27:15 +1000

    Finish Configure Router

--- REMP/src/App.tsx
+++ REMP/src/App.tsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,7 +17,7 @@ function App() {
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo react" alt="React logo" /> npm install react-router-dom
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -29,7 +32,38 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <BrowserRouter>
+      <div className="p-4">
+        <nav className="space-x-4 mb-6">
+          <h2>测试Route</h2>
+          
+
+         
+        </nav>
+
+        <Routes>
+          <Route path="/" element={
+            <div>
+              <Link to="/About" className="text-blue-500 hover:underline">
+              <button style={{padding:'10px 20px', fontSize:'16px'}}>Navigate to the about Page</button>
+              </Link>
+              <Home/>
+            </div>
+          } />
+
+          <Route path="/about" element={
+            <div>
+              <Link to="/" className="text-blue-500 hover:underline">
+              <button style={{padding:'10px 20px', fontSize:'16px'}}>Navigate to the Home Page</button>
+              </Link>
+              <About/>
+            </div>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
     </>
+    
   )
 }
 
---
+++ REMP/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+type Props = {};
+
+function About({}: Props) {
+  return <h2 className="text-xl font-semibold text-purple-600">这是主页</h2>;
+}
+
+export default About;
