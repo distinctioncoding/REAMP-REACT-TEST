f451e52c28aabaf56d9fc09f9c34e6e69e53b1bb
Elijah925 <79500501+Elijah925@users.noreply.github.com>
2025-05-03 20:29:20 +1000

    update router

--- REMP/src/pages/About.tsx
+++
@@ -1,7 +0,0 @@
-type Props = {};
-
-function About({}: Props) {
-  return <h2 className="text-xl font-semibold text-purple-600">这是关于我们页面</h2>;
-}
-
-export default About;
--- REMP/src/pages/Home.tsx
+++
@@ -1,7 +0,0 @@
-type Props = {};
-
-function Home({}: Props) {
-  return <h2 className="text-xl font-semibold text-purple-600">这是主页</h2>;
-}
-
-export default Home;
