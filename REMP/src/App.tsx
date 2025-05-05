7158c26ed0d1e976ae37de104f173e2537eada17
depeng zhang <zhangdepeng3@gmail.com>
2025-05-05 15:17:24 +1000

    ream-154

--- REMP/src/App.tsx
+++ REMP/src/App.tsx
@@ -1,14 +1,15 @@
-import './App.css'
+import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import PropertyCard from './components/PropertyCard'; 
+
 function App() {
   return (
     <BrowserRouter>
-      {/* 你可以稍后在这里添加 <Routes> 和 <Route> */}
       <Routes>
-          <Route path="/"/>
-        </Routes>
+        <Route path="/property" element={<PropertyCard />} />
+      </Routes>
     </BrowserRouter>
   );
 }
 
-export default App
+export default App;
