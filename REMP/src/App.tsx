112fe0ce7582ffae470ef568ad0c0b1d7074a0bd
Yuntannn <jonathan2014max@gmail.com>
2025-05-08 20:37:56 +1000

    feat(REAM-139): Integrate SF Pro Display font with five weights and apply global styles

--- REMP/src/App.tsx
+++ REMP/src/App.tsx
@@ -5,8 +5,17 @@ function App() {
     <BrowserRouter>
       {/* 你可以稍后在这里添加 <Routes> 和 <Route> */}
       <Routes>
-          <Route path="/"/>
-        </Routes>
+        <Route path="/" element={
+          <>
+          {/* test code */}
+            <h1 style={{ fontFamily: 'SF Pro Display', fontWeight: 200 }}>Thin Weight (200)</h1>
+            <h1 style={{ fontFamily: 'SF Pro Display', fontWeight: 400 }}>Regular (400)</h1>
+            <h1 style={{ fontFamily: 'SF Pro Display', fontWeight: 500 }}>Medium (500)</h1>
+            <h1 style={{ fontFamily: 'SF Pro Display', fontWeight: 600 }}>Semibold (600)</h1>
+            <h1 style={{ fontFamily: 'SF Pro Display', fontWeight: 700 }}>Bold (700)</h1>
+          </>
+        } />
+      </Routes>
     </BrowserRouter>
   );
 }
