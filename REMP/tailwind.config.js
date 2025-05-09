861ef33dd38aadf5e4a603073155e59ccad26431
Yuntannn <jonathan2014max@gmail.com>
2025-05-09 15:06:52 +1000

    fix: remove unnecessary code

--- REMP/tailwind.config.js
+++ REMP/tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
     theme: {
       extend: {
         fontFamily: {
-          sans: ['"SF Pro Display"', 'system-ui', 'sans-serif'],
+          sans: ['SF Pro Display', 'system-ui', 'sans-serif'],
         },
       },
     },
