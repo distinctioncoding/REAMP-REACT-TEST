3a259bb4ce43e65c25e47d355d6056335c1cfcf2
depeng zhang <zhangdepeng3@gmail.com>
2025-05-12 23:22:36 +1000

    add login components

--- REMP/src/App.tsx
+++ REMP/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import SignInPage from './components/SignInPage';
 import { AgentPropertyPage } from './components/AgentPropertyPage/AgentPropertyPage';
 
 // mock user token for testing
@@ -34,6 +34,7 @@ function App() {
 
 
         <Route path="/AgentPropertyPage" element={<AgentPropertyPage/>} />
+        <Route path="/login" element={<SignInPage/>} />
 
 
         <Route
